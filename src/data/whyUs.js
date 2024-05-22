7c9023d8aafaf5cb1ce8c389e5c38386b5e426c2
fajarikas <fajarika.2021@student.uny.ac.id>
2024-05-22 16:24:06 +0700

    some changes

--- src/data/whyUs.js
+++ src/data/whyUs.js
@@ -1,25 +1,27 @@
+import VerifiedCheckIcon from "../../public/assets/images/image-icon/L-Verified Check.png";
+import UpArrowIcon from "../../public/assets/images/image-icon/panah-atas.png";
+import NotificationsIcon from "../../public/assets/images/image-icon/L-Notifications.png";
+import BookmarkIcon from "../../public/assets/images/image-icon/L-Bookmark.png";
+
 export const whyUs = [
     {
         "id" : 1,
-        "image" : "../../../public/assets/images/image-icon/L-Verified Check.png",
+        "image" : VerifiedCheckIcon,
         "title" : "Informasi Lomba yang Lengkap",
     },
     {
         "id" : 2,
-        "image" : "../../../public/assets/images/image-icon/panah-atas.png",
-
+        "image" : UpArrowIcon,
         "title" : "Upload Lomba dengan Mudah",
     },
     {
         "id" : 3,
-        "image" : "../../../public/assets/images/image-icon/L-Notifications.png",
-
+        "image" : NotificationsIcon,
         "title" : "Memberikan Notifikasi Event Baru",
     },
     {
         "id" : 4,
-        "image" : "../../../public/assets/images/image-icon/L-Bookmark.png",
-
+        "image" : BookmarkIcon,
         "title" : "Penyimpanan Event Favorite",
     }
 ]
